b4646bc586ae7ee2cbea353f8a1ede4e35498bdb
Matheus <matheeus161@gmail.com>
2024-12-24 09:27:41 -0300

    feature: using useGlobalPipes, ValidationPipes and class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   /* Carrega o módulo raiz da aplicação */
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
