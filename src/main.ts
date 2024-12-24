5f0c1d6cdb525c38b1e86ebf777361a69129bedd
Matheus <matheeus161@gmail.com>
2024-12-24 10:02:23 -0300

    feature: using whitelist and forbidNonWhitelisted to block malicious properties

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   /* Carrega o módulo raiz da aplicação */
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove keys that aren't defined the DTO
+      forbidNonWhitelisted: true, // throw an error if a property not exist in the DTO
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
