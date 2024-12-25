3611068b13fb5d13ad07ab4b9d561e19f50f46bb
Matheus <matheeus161@gmail.com>
2024-12-25 11:56:09 -0300

    feat: configure database connection with TypeORM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from 'src/messages/messages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 /* Organizar e encapsular o código */
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      database: 'postgres',
+      password: '123456',
+      autoLoadEntities: true, // Carrega entidades sem precisar especifica-las
+      synchronize: true, // Sincroniza com o BD. Não deve ser usado em produção
+    }),
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService] /* Injetar dependencias */,
 })
