e4ea367adcc4d4301a83e7796b4d7b4b6edf5724
Matheus <matheeus161@gmail.com>
2024-12-25 14:58:02 -0300

    feature: findAll, findOne and remove people methods

--- src/people/people.service.ts
+++ src/people/people.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -32,19 +36,39 @@ export class PeopleService {
     }
   }
 
-  findAll() {
-    return `This action returns all people`;
+  async findAll() {
+    const people = await this.personRepository.find({
+      order: {
+        id: 'desc',
+      },
+    });
+
+    return people;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} person`;
+  async findOne(id: number) {
+    const person = await this.personRepository.findOneBy({
+      id,
+    });
+
+    if (!person) {
+      throw new NotFoundException('Pessoa não encontrada');
+    }
+
+    return person;
   }
 
   update(id: number, updatePersonDto: UpdatePersonDto) {
     return `This action updates a #${id} person`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} person`;
+  async remove(id: number) {
+    const person = await this.personRepository.findOneBy({ id });
+
+    if (!person) {
+      throw new NotFoundException('Pessoa não encontrada');
+    }
+
+    return this.personRepository.remove(person);
   }
 }
