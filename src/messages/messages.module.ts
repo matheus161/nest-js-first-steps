b8a69fd345c6ff593b3b04e39874046bdb092c28
Matheus <matheeus161@gmail.com>
2024-12-27 08:49:59 -0300

    refactor: handle with circular dependency

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,10 +7,19 @@ import { PeopleModule } from 'src/people/people.module';
 import { MessageUtils } from './message.utils';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message]), PeopleModule], // Import entities for this module
+  imports: [
+    TypeOrmModule.forFeature([Message]),
+    forwardRef(() => PeopleModule),
+  ], // Import entities for this module
   controllers: [MessagesController],
   providers: [
     MessagesService,
+    {
+      provide: MessageUtils,
+      useClass: MessageUtils,
+    },
+  ],
+  exports: [
     {
       provide: MessageUtils,
       useClass: MessageUtils,
--- src/people/people.controller.ts
+++ src/people/people.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { PeopleService } from './people.service';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
+import { MessageUtils } from 'src/messages/message.utils';
 
 @Controller('people')
 export class PeopleController {
-  constructor(private readonly peopleService: PeopleService) {}
+  constructor(
+    private readonly peopleService: PeopleService,
+    private readonly messageUtils: MessageUtils,
+  ) {}
 
   @Post()
   create(@Body() createPersonDto: CreatePersonDto) {
@@ -14,6 +26,7 @@ export class PeopleController {
 
   @Get()
   findAll() {
+    console.log(this.messageUtils.inverteString('Matheus'));
     return this.peopleService.findAll();
   }
 
