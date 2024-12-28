99f36901823959576238a0ece2b5b3859ca1f50a
Matheus <matheeus161@gmail.com>
2024-12-28 12:28:54 -0300

    feature: add environment variables in database connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,21 @@ import { AppService } from './app.service';
 import { MessagesModule } from 'src/messages/messages.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PeopleModule } from 'src/people/people.module';
+import { ConfigModule } from '@nestjs/config';
 
 /* Organizar e encapsular o código */
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      database: 'postgres',
-      password: '123456',
-      autoLoadEntities: true, // Loads entities without needing to specify them
-      synchronize: true, // Synchronizes with the database. Should not be used in production
+      type: process.env.DATABASE_TYPE as 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      database: process.env.DATABASE_DATABASE,
+      password: process.env.DATABASE_PASSWORD,
+      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES), // Loads entities without needing to specify them
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // Synchronizes with the database. Should not be used in production
     }),
     MessagesModule,
     PeopleModule,
