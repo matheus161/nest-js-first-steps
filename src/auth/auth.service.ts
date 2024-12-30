874605fec5ed37284f832b72040c411f82312cf4
Matheus <matheeus161@gmail.com>
2024-12-30 10:55:43 -0300

    refactor: add login validations

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { Repository } from 'typeorm';
+import { Person } from 'src/people/entities/person.entity';
+import { HashingService } from './hashing/hashing.service';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(Person)
+    private readonly personRepository: Repository<Person>,
+    private readonly hashingService: HashingService,
+  ) {}
+
   async login(loginDto: LoginDto) {
-    return loginDto;
+    const person = await this.personRepository.findOneBy({
+      email: loginDto.email,
+    });
+
+    if (!person) {
+      throw new UnauthorizedException('Usuário ou senha inválidos.');
+    }
+
+    const passwordIsValid = await this.hashingService.compare(
+      loginDto.password,
+      person.passwordHash,
+    );
+
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Usuário ou senha inválidos.');
+    }
+
+    return { message: 'Usuário logado!' };
   }
 }
