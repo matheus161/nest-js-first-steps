7f6d9b20540480541dc1352756f9a89591c1b2f3
Matheus <matheeus161@gmail.com>
2024-12-30 11:08:03 -0300

    feature: add jwt configuration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { Repository } from 'typeorm';
 import { Person } from 'src/people/entities/person.entity';
 import { HashingService } from './hashing/hashing.service';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ConfigType } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
 
 @Injectable()
 export class AuthService {
@@ -11,7 +13,11 @@ export class AuthService {
     @InjectRepository(Person)
     private readonly personRepository: Repository<Person>,
     private readonly hashingService: HashingService,
-  ) {}
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {
+    console.log(jwtConfiguration);
+  }
 
   async login(loginDto: LoginDto) {
     const person = await this.personRepository.findOneBy({
