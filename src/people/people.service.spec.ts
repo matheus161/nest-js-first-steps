59af71be332e782bedeff842d0101fbd4d3d7d3b
Matheus <matheeus161@gmail.com>
2025-01-08 08:38:31 -0300

    feat: use jest.fn and jest.spyOn to simulate method return calls

--- src/people/people.service.spec.ts
+++ src/people/people.service.spec.ts
@@ -4,6 +4,8 @@ import { Person } from './entities/person.entity';
 import { HashingService } from 'src/auth/hashing/hashing.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreatePersonDto } from './dto/create-person.dto';
+import { RoutePolicies } from 'src/auth/enum/route-policies.enum';
 
 describe('PessoasService', () => {
   let peopleService: PeopleService;
@@ -16,11 +18,16 @@ describe('PessoasService', () => {
         PeopleService,
         {
           provide: getRepositoryToken(Person),
-          useValue: {},
+          useValue: {
+            save: jest.fn(),
+            create: jest.fn(),
+          },
         },
         {
           provide: HashingService,
-          useValue: {},
+          useValue: {
+            hash: jest.fn(),
+          },
         },
       ],
     }).compile();
@@ -34,5 +41,41 @@ describe('PessoasService', () => {
 
   it('pessoaService deve estar definido', () => {
     expect(peopleService).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a new person', async () => {
+      // Arange
+      // CreatePessoaDto
+      const createPersonDto: CreatePersonDto = {
+        email: 'matheus@email.com',
+        nome: 'Matheus',
+        password: '123456',
+        routePolicies: [RoutePolicies.createPessoa],
+      };
+      const passwordHash = 'HASHDESENHA';
+
+      // Que o hashing service tenha o método hash
+      // Saber se o hashing service foi chamado com CreatePessoaDto
+      // Saber se o pessoaRepository.create foi chamado com dados pessoa
+      // Saber se pessoaRepository.save foi chamado com a pessoa criada
+      // O retorno final deve ser a nova pessoa criada -> expect
+
+      jest.spyOn(hashingService, 'hash').mockResolvedValue(passwordHash);
+
+      // Act
+      await peopleService.create(createPersonDto);
+
+      // Assert
+      expect(hashingService.hash).toHaveBeenCalledWith(
+        createPersonDto.password,
+      );
+      expect(personRepository.create).toHaveBeenCalledWith({
+        nome: createPersonDto.nome,
+        passwordHash,
+        email: createPersonDto.email,
+        routePolicies: [RoutePolicies.createPessoa],
+      });
+    });
   });
 });
