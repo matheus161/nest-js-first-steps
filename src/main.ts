af1a4fc29817bab58246011e71eab3ac79fe54ad
Matheus <matheeus161@gmail.com>
2025-01-22 14:20:47 -0300

    feat: configure swagger document generation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import appConfig from './app/config/app.config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   /* Carrega o módulo raiz da aplicação */
   const app = await NestFactory.create(AppModule);
 
   appConfig(app);
+
+  const documentBuilderConfig = new DocumentBuilder()
+    .setTitle('Recados API')
+    .setDescription('Envie recados para seus amigos e familiares')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentBuilderConfig);
+
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
